a58bbea350e4e8a03c9085fd37fd9898978de71e
JethNo <jethromartin4619@gmail.com>
2023-03-21 17:10:58 +0800

    second commit test

---
+++ indexActivity.js
@@ -0,0 +1,10 @@
+console.log("Hello, Welcome to De La Salle University!");
+var address = "No. 5, Rolling Street, Houseville, Barangay Krushno, Kansas City, East Lands, 602947";
+var user = {
+	firstName: "Jethro Martin",
+	lastName: "Moron",
+	age: "20",
+	hobbies: ["writing, reading, listening to music, going to the gym, eating"],
+	homeAddress: address
+}
+console.log(user);
